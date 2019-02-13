b29b5309021cf70e05e5ea73d6ed75f7f02f4771
Blake Hardesty <ha1003218@email.ccbcmd.edu>
2019-02-13 01:48:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementById("nested").querySelector(".target");
+}
+
+function deepestChild(){
+  var lis=document.getElementById('grand-node').querySelectorAll("div");
+  var test;
+  for (let i=0;i<lis.length-1;i++){
+    test=lis[i].querySelector("div");
+  }
+  return test;
+}
+
+function increaseRankBy(n){
+var ranks = document.querySelectorAll('ul.ranked-list');
+for (let i=0; i<ranks.length;i++){
+  ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+}
+}
